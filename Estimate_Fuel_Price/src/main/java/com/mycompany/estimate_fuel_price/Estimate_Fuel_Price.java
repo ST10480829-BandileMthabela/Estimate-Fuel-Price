51dae6a60a2bc1ac158c61c25a580a12233d42c3
ST10480829-BandileMthabela <bobmthabela@gmail.com>
2026-03-24 16:15:27 +0200

    added calculations and show message

--- Estimate_Fuel_Price/src/main/java/com/mycompany/estimate_fuel_price/Estimate_Fuel_Price.java
+++ Estimate_Fuel_Price/src/main/java/com/mycompany/estimate_fuel_price/Estimate_Fuel_Price.java
@@ -6,24 +6,8 @@ import java.util.Scanner;
 
 public class Estimate_Fuel_Price {
 
-    public static void main(String[] args) 
-    {
+    public static void main(String[] args) {
     Scanner keep = new Scanner(System.in);
-
-    System.out.print("Please enter the the distance.");
-        double distance = keep.nextDouble();
-        
-    System.out.print("Fuel efficiency (Km/l)");
-        double fuelEfficiancy = keep.nextDouble();
-        
-    System.out.print("Price of fuel per leter.");
-        double pricePerFuel = keep.nextDouble();
-
-    System.out.println("=================== Estimate Fuel Price ===================");
-    System.out.println("Total Cost: R " + distance );
-    System.out.println("Fuel Efficiency: R" + fuelEfficiancy);
-    System.out.println("Price of price: R" + pricePerFuel);
-    System.out.println("===========================================================");  
     }
    
 }
