cf23803c32880bcba0c0fd2091ed3a1749044f36
ST10480829-BandileMthabela <bobmthabela@gmail.com>
2026-03-24 14:46:30 +0200

    Initial commit - ICE Task 1

---
+++ Estimate_Fuel_Price/src/main/java/com/mycompany/estimate_fuel_price/Estimate_Fuel_Price.java
@@ -0,0 +1,13 @@
+// ST10480829 Bandile Mthabela
+// ICE Task 1
+package com.mycompany.estimate_fuel_price;
+
+import java.util.Scanner;
+
+public class Estimate_Fuel_Price {
+
+    public static void main(String[] args) {
+    Scanner keep = new Scanner(System.in);
+    }
+   
+}
